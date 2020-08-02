d1357c4d949c02d121c329b92e5d9d1a856f5da8
Alex Sandri <me@alexsandri.com>
2020-08-02 18:42:07 +0200

    Update the service worker on updateready

--- src/routes/index/index.route.ts
+++ src/routes/index/index.route.ts
@@ -2,3 +2,5 @@ import * as pizza from "@alex-sandri/pizza-utilities";
 import { ClasseViva } from "@alex-sandri/classeviva-api";
 
 pizza.ServiceWorker.register();
+
+pizza.ServiceWorker.listen("updateready", pizza.ServiceWorker.update);
--- src/routes/offline/offline.route.ts
+++ src/routes/offline/offline.route.ts
@@ -1,3 +1,5 @@
 import * as pizza from "@alex-sandri/pizza-utilities";
 
 pizza.ServiceWorker.register();
+
+pizza.ServiceWorker.listen("updateready", pizza.ServiceWorker.update);
