ada6cfb94fe7bbe5816b1433d4feae14164081de
Alex Sandri <me@alexsandri.com>
2020-08-02 19:48:13 +0200

    Create session on form submit

--- src/routes/index/index.route.ts
+++ src/routes/index/index.route.ts
@@ -4,3 +4,15 @@ import { ClasseViva } from "@alex-sandri/classeviva-api";
 pizza.ServiceWorker.register();
 
 pizza.ServiceWorker.listen("updateready", pizza.ServiceWorker.update);
+
+const signInForm = <HTMLFormElement>document.querySelector("form");
+
+const uidInput = <HTMLInputElement>document.querySelector("#uid");
+const pwdInput = <HTMLInputElement>document.querySelector("#pwd");
+
+signInForm.addEventListener("submit", async e =>
+{
+    e.preventDefault();
+
+    await ClasseViva.createSession(uidInput.value, pwdInput.value);
+});
